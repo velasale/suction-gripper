f7aab72f2838614a5ed1020e90f21004cc48007c
Alejo <velasale@oregonstate.edu>
2023-05-12 16:53:35 -0700

    suction gripper exeriments layout

---
+++ src/performance_test.py
@@ -0,0 +1,36 @@
+
+
+
+def main():
+
+    # TODO modify joint_limits.yaml for this test
+    # TODO update the environment and scene
+    # TODO place camera on Apple Proxy
+    # TODO organize electronics of apple proxy
+    #
+    # Initialize Class
+
+    # --- Step 1: Place robot at waypoint (preliminary position)
+
+    # --- Step 2: Obtain info from user
+    # Info about gripper: pressure
+    # Info about apple: stiffness -- type of springs, apples used
+
+    # --- Step 3: Check that the vacuum circuit is free of holes
+    # Turn on valve
+    # Place flat object
+    # Plot and check all sensors are @20KPa
+    # Check camera, and all signals
+
+    # --- Step 4: Pick the desired experiment
+    #
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+
