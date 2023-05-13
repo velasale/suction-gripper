83bd4c9416638b29fd3e4197bf13c70c64f1d9fb
Alejo <velasale@oregonstate.edu>
2023-05-12 17:42:00 -0700

    suction gripper exeriments layout

--- src/performance_test.py
+++ src/performance_test.py
@@ -7,7 +7,7 @@ def main():
     # TODO update the environment and scene
     # TODO place camera on Apple Proxy
     # TODO organize electronics of apple proxy
-    #
+
     # Initialize Class
 
     # --- Step 1: Place robot at waypoint (preliminary position)
@@ -15,6 +15,7 @@ def main():
     # --- Step 2: Obtain info from user
     # Info about gripper: pressure
     # Info about apple: stiffness -- type of springs, apples used
+    # Pass these properties to the class
 
     # --- Step 3: Check that the vacuum circuit is free of holes
     # Turn on valve
@@ -23,7 +24,75 @@ def main():
     # Check camera, and all signals
 
     # --- Step 4: Pick the desired experiment
-    #
+    # Exp 1: Real Apples
+    # Exp 2: Apple Proxy
+
+    ...
+
+
+def proxy_picks():
+
+    # --- Experiment Parameters ---
+    # Number of experiments
+    # Amount of noise
+
+        # Measure Apple Position (simply add a fiducial marker)
+
+        # Move to Starting Position
+
+        # Start Recording Rosbag file
+
+        # Add noise
+
+        # Open Valve (apply vacuum)
+
+        # Approach Surface
+
+        # Label number of cups
+
+        # Retreieve
+
+        # Label result
+
+        # Close Valve (stop vacuum)
+
+        # Stop Recording Rosbag file
+
+        # Save metadata in yaml file
+
+        # Plot results, and decide to toss experiment away or not
+
+
+
+    ...
+
+
+def real_picks():
+    ...
+
+
+
+class RoboticGripper():
+
+    def __init__(self):
+
+    def go_to_preliminary_position(self):
+
+    def go_to_starting_position(self):
+
+    def add_cartesian_noise(self):
+
+    def place_marker_text(self):
+
+    def save_metadata(self):
+
+    def publish_event(self):
+
+    def check_real_noise(self):
+
+
+
+
 
 
 
