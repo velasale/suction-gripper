07eeec08af98654c30a027d666b64bb063bad4ed
Alejo <velasale@oregonstate.edu>
2023-07-18 15:17:52 -0700

    simple script to test functions and methods from main files

---
+++ src/arena.py
@@ -0,0 +1,44 @@
+from performance_test import *
+# --- Standard Library Imports
+import copy
+import csv
+import math
+import matplotlib.pyplot as pp
+import mpl_toolkits.mplot3d
+
+import numpy as np
+from numpy import pi, cos, sin, arccos, arange
+import os
+import keyboard
+from random import random
+import rospy
+import time
+import statistics as st
+import sys
+import rosbag
+import json
+import datetime
+
+## --- 3rd party related imports
+import geometry_msgs.msg
+from geometry_msgs.msg import Pose, Point, Quaternion, Vector3, Polygon
+import moveit_commander
+import moveit_msgs.msg
+from std_msgs.msg import String, Int32, UInt16
+import tf
+from tf.transformations import euler_from_quaternion, quaternion_about_axis, quaternion_from_euler
+import tf2_ros
+import tf2_geometry_msgs  # **Do not use geometry_msgs. Use this instead for PoseStamped
+from visualization_msgs.msg import Marker, MarkerArray
+
+## --- Self developed imports
+from pressure_plot_scripts import *
+from ros_scripts import *
+from plot_scripts import *
+
+gripper = RoboticGripper()
+
+gripper.point_sampling()
+
+
+
