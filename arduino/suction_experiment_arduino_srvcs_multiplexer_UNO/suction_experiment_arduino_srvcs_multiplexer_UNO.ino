45f54e82712106b18b8cf65a57c55c2b3ed37317
Alejo <velasale@oregonstate.edu>
2023-07-27 10:05:56 -0700

    Fixed location of robot w.r.t small table

---
+++ arduino/suction_experiment_arduino_srvcs_multiplexer_UNO/suction_experiment_arduino_srvcs_multiplexer_UNO.ino
@@ -0,0 +1,203 @@
+/*!
+   @file mprls_simpletest.ino
+
+   A basic test of the sensor with default settings
+
+   Designed specifically to work with the MPRLS sensor from Adafruit
+   ----> https://www.adafruit.com/products/3965
+
+   These sensors use I2C to communicate, 2 pins (SCL+SDA) are required
+   to interface with the breakout.
+
+   Adafruit invests time and resources providing this open source code,
+   please support Adafruit and open-source hardware by purchasing
+   products from Adafruit!
+
+   Written by Limor Fried/Ladyada for Adafruit Industries.
+
+   MIT license, all text here must be included in any redistribution.
+
+*/
+
+
+const bool  USE_ROSSERIAL = true;
+
+#include <Wire.h>
+#include <time.h>
+#include "Adafruit_MPRLS.h"
+
+//You don't *need* a reset and EOC pin for most usees, so we set to -1 and don't connect
+#define RESET_PIN -1
+#define EOC_PIN   -1
+#define VALVE 13
+Adafruit_MPRLS mpr = Adafruit_MPRLS(RESET_PIN, EOC_PIN);
+
+
+
+
+
+
+
+/*************************** Base ROS Setup ******************************/
+// General ROS packages/nodes
+#include <ros.h>
+ros::NodeHandle nh;
+
+
+
+
+
+/*************************** ROS Services Setup **************************/
+#include <std_srvs/Trigger.h>
+void closeValveService(const std_srvs::Trigger::Request &req,
+                       std_srvs::TriggerResponse &res) {
+  res.success = closeValve();
+}
+ros::ServiceServer<std_srvs::Trigger::Request,
+    std_srvs::Trigger::Response>
+    service_close("closeValve", &closeValveService);
+
+
+void openValveService(const std_srvs::Trigger::Request &req,
+                      std_srvs::TriggerResponse &res) {
+  res.success = openValve();
+}
+ros::ServiceServer<std_srvs::Trigger::Request,
+    std_srvs::Trigger::Response>
+    service_open("openValve", &openValveService);
+
+
+
+
+/*************************** ROS Publishers Setup **************************/
+#include <std_msgs/Float32.h>
+#include <std_msgs/UInt16.h>
+#include <std_msgs/UInt8.h>
+
+
+std_msgs::UInt16 press_msg[3];
+ros::Publisher publisher_pressure[3] {
+  ros::Publisher("/gripper/pressure/sc1", &press_msg[0]),
+  ros::Publisher("/gripper/pressure/sc2", &press_msg[1]),
+  ros::Publisher("/gripper/pressure/sc3", &press_msg[2])
+};
+
+
+// Constants
+const int PCAADR = 0x70;
+const int VALVE_DELAY = 10;
+const int PUBLISH_DELAY = 2;
+
+
+
+/**************************** Setup ***************************************/
+void setup() {
+  // initialize serial:
+  if (!USE_ROSSERIAL){ 
+    Serial.begin(57600);     // 57600, 76800, 115200    
+  }
+  
+  // Initialize VALVE pin as output
+  delay(10);
+  pinMode(VALVE, OUTPUT);
+  delay(10);
+  digitalWrite(VALVE, LOW);
+  delay(10);
+
+  // Initialize pressure sensor
+  mpr.begin();
+
+  // Initialize ROS stuff
+  if (USE_ROSSERIAL) {
+
+    // Initialize ROS node
+//    nh.getHardware()->setBaud(250000);     //Needs to match the one set in the PC's launch file
+    nh.initNode();
+
+    // Initialize ROS publishers
+    for (uint8_t i = 0 ; i < 3; i++) {
+      nh.advertise(publisher_pressure[i]);
+    }
+    
+    // ROS services
+    nh.advertiseService(service_open);
+    nh.advertiseService(service_close);
+  }
+
+  
+
+}
+
+
+/***************************** Loop ****************************************/
+void loop() {
+  
+  unsigned long currentMillis;
+  uint16_t pressure_hPa;
+
+
+  if (USE_ROSSERIAL) {
+    nh.spinOnce();
+  }
+  else{
+    Serial.println("\n......I2C Multiplexing.......");
+  } 
+  
+  for (uint8_t i = 0; i < (3 + 1); i++) {
+    // NUM_CUPS +1 is because the i2c multiplexer reads the three pressure sensors AND the
+    // time of Flight sensor        
+
+    currentMillis = millis();
+
+    pcaselect(i);
+    
+    // Measure Pressure in the first three (3) channels 
+    if(i<3){
+      pressure_hPa = mpr.readPressure();
+      press_msg[i].data = pressure_hPa;      
+
+      if (USE_ROSSERIAL) {
+        publisher_pressure[i].publish(&press_msg[i]);
+        delay(PUBLISH_DELAY);
+      }
+      else {
+        //Print the time in ms just to have an idea of long the sensor takes to measure press.
+        Serial.println("[Ch" + String(i) +"] " + "Period: " + String(millis() - currentMillis) + " ms, " + "Pressure: " + String(press_msg[i].data) + " hPa");     
+      }
+      
+    }
+      
+
+  }
+
+  
+
+}
+
+/****************************** Control Functions ***************************/
+bool closeValve() {
+  bool success = true;
+  delay(VALVE_DELAY);
+  digitalWrite(VALVE, LOW);
+  delay(VALVE_DELAY);
+  return success;
+}
+
+
+bool openValve() {
+  bool success = true;
+  delay(VALVE_DELAY);
+  digitalWrite(VALVE, HIGH);
+  delay(VALVE_DELAY);
+  return success;
+}
+
+/**************************** I2C Multiplexer *********************************/
+// Reference: https://learn.adafruit.com/adafruit-pca9546-4-channel-i2c-multiplexer/arduino
+void pcaselect(uint8_t i) {
+  if (i > 4) return;
+
+  Wire.beginTransmission(PCAADR);
+  Wire.write(1 << i);
+  Wire.endTransmission();
+}
