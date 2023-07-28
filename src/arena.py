81f859f2b91c56633a3ab3c669bdef84825412e4
Alejo Velasquez <velasale@oregonstate.edu>
2023-07-27 22:01:20 -0700

    bag_to_pngs

--- src/arena.py
+++ src/arena.py
@@ -1,4 +1,3 @@
-from performance_test import *
 # --- Standard Library Imports
 import copy
 import csv
@@ -9,36 +8,26 @@ import mpl_toolkits.mplot3d
 import numpy as np
 from numpy import pi, cos, sin, arccos, arange
 import os
-import keyboard
 from random import random
-import rospy
+
 import time
 import statistics as st
 import sys
-import rosbag
+import pyrosbag
 import json
 import datetime
 
 ## --- 3rd party related imports
-import geometry_msgs.msg
-from geometry_msgs.msg import Pose, Point, Quaternion, Vector3, Polygon
-import moveit_commander
-import moveit_msgs.msg
-from std_msgs.msg import String, Int32, UInt16
-import tf
-from tf.transformations import euler_from_quaternion, quaternion_about_axis, quaternion_from_euler
-import tf2_ros
-import tf2_geometry_msgs  # **Do not use geometry_msgs. Use this instead for PoseStamped
-from visualization_msgs.msg import Marker, MarkerArray
+
 
 ## --- Self developed imports
-from pressure_plot_scripts import *
+
 from ros_scripts import *
-from plot_scripts import *
-
-gripper = RoboticGripper()
-
-gripper.point_sampling_2d()
 
 
+folder = "D:/"
+file = "20230724_proxy_sample_0_yaw_0_rep_0_stiff_high_force_low.bag"
+topic = "/usb_cam/image_raw"
+
+bag_to_pngs(folder, file, topic)
 
--- src/ros_scripts.py
+++ src/ros_scripts.py
@@ -10,31 +10,35 @@ import datetime
 # --- 3rd party imports
 import rospy
 import geometry_msgs.msg
-from moveit_commander.conversions import pose_to_list
-from visualization_msgs.msg import Marker, MarkerArray
+# from moveit_commander.conversions import pose_to_list
+# from visualization_msgs.msg import Marker, MarkerArray
+
+import cv2
+import rosbag
+from cv_bridge import CvBridge
 
 
-def all_close(goal, current, tolerance):
-    """
-    Convenient method for testing if a lits of values are within a tolerance
-    @param goal:
-    @param current:
-    @param tolerance:
-    @return:
-    """
-    if type(goal) is list:
-        for index in range(len(goal)):
-            if abs(current[index] - goal[index]) > tolerance:
-                return False
-
-    elif type(goal) is geometry_msgs.msg.PoseStamped:
-        # TODO difference between Pose and PoseStamped
-        return all_close(goal.pose, current.pose, tolerance)
-
-    elif type(goal) is geometry_msgs.msg.Pose:
-        return all_close(pose_to_list(goal), pose_to_list(current), tolerance)
-
-    return True
+# def all_close(goal, current, tolerance):
+#     """
+#     Convenient method for testing if a lits of values are within a tolerance
+#     @param goal:
+#     @param current:
+#     @param tolerance:
+#     @return:
+#     """
+#     if type(goal) is list:
+#         for index in range(len(goal)):
+#             if abs(current[index] - goal[index]) > tolerance:
+#                 return False
+#
+#     elif type(goal) is geometry_msgs.msg.PoseStamped:
+#         # TODO difference between Pose and PoseStamped
+#         return all_close(goal.pose, current.pose, tolerance)
+#
+#     elif type(goal) is geometry_msgs.msg.Pose:
+#         return all_close(pose_to_list(goal), pose_to_list(current), tolerance)
+#
+#     return True
 
 
 def service_call(service):
@@ -45,118 +49,118 @@ def service_call(service):
 
 
 # ---------------- ROS MARKER FUNCTIONS ------------------------------#
-def place_marker_text(pos=[0, 0, 0], scale=0.01, text='caption', cframe='world'):
-    """
-    Places text as marker in RVIZ
-    @param pos: x,y,z location
-    @param scale: scale of the text
-    @param text: text to display
-    @param cframe: cframe on which the coordinates are given
-    @return:
-    """
-    # Create a marker.  Markers of all shapes share a common type.
-    caption = Marker()
-
-    # Set the frame ID and type.  The frame ID is the frame in which the position of the marker
-    # is specified.  The type is the shape of the marker, detailed on the wiki page.
-    caption.header.frame_id = cframe
-    caption.type = caption.TEXT_VIEW_FACING
-
-    # Each marker has a unique ID number.  If you have more than one marker that you want displayed at a
-    # given time, then each needs to have a unique ID number.  If you publish a new marker with the same
-    # ID number and an existing marker, it will replace the existing marker with that ID number.
-    caption.id = 0
-
-    # Set the action.  We can add, delete, or modify markers.
-    caption.action = caption.ADD
-
-    # These are the size parameters for the marker.  The effect of these on the marker will vary by shape,
-    # but, basically, they specify how big the marker along each of the axes of the coordinate frame named
-    # in frame_id.
-    caption.scale.x = scale
-    caption.scale.y = scale
-    caption.scale.z = scale
-
-    # Color, as an RGB triple, from 0 to 1.
-    caption.color.r = 0
-    caption.color.g = 0
-    caption.color.b = 0
-    caption.color.a = 1
-
-    caption.text = text
-
-    # Specify the pose of the marker.  Since spheres are rotationally invarient, we're only going to specify
-    # the positional elements.  As usual, these are in the coordinate frame named in frame_id.  Every time the
-    # marker is displayed in rviz, ROS will use tf to determine where the marker should appear in the scene.
-    # in this case, the position will always be directly above the robot, and will move with it.
-    caption.pose.position.x = pos[0]
-    caption.pose.position.y = pos[1]
-    caption.pose.position.z = pos[2]
-
-    # Set up a publisher.  We're going to publish on a topic called balloon.
-    self.marker_text_publisher.publish(caption)
-
-    # Set a rate.  10 Hz is a good default rate for a marker moving with the Fetch robot.
-    rate = rospy.Rate(10)
-
-
-def place_marker(color=[0, 0, 1, 1], pos=[0, 0, 0], scale=0.01, cframe='world'):
-    """
-    Creates a Sphere as Marker, and appends it into an array of markers
-    @param color: [r,g,b,a] where rgb are color i RGB format, and 'a' is visibility
-    @param pos: [x,y,z] coordinates of the center of the marker
-    @param scale:
-    @param cframe: coordinate frame
-    @return:
-    """
-    # Create a marker.  Markers of all shapes share a common type.
-    sphere = Marker()
-
-    # Set the frame ID and type.  The frame ID is the frame in which the position of the marker
-    # is specified.  The type is the shape of the marker, detailed on the wiki page.
-    sphere.header.frame_id = cframe
-    sphere.type = sphere.SPHERE
-
-    # Each marker has a unique ID number.  If you have more than one marker that you want displayed at a
-    # given time, then each needs to have a unique ID number.  If you publish a new marker with the same
-    # ID number and an existing marker, it will replace the existing marker with that ID number.
-    sphere.id = self.marker_id + 1
-    self.marker_id = sphere.id
-
-    # Set the action.  We can add, delete, or modify markers.
-    sphere.action = sphere.ADD
-
-    # These are the size parameters for the marker.  The effect of these on the marker will vary by shape,
-    # but, basically, they specify how big the marker along each of the axes of the coordinate frame named
-    # in frame_id.
-    sphere.scale.x = scale
-    sphere.scale.y = scale
-    sphere.scale.z = scale
-
-    # Color, as an RGB triple, from 0 to 1.
-    sphere.color.r = color[0]
-    sphere.color.g = color[1]
-    sphere.color.b = color[2]
-    sphere.color.a = color[3]
-
-    # Specify the pose of the marker.  Since spheres are rotationally invarient, we're only going to specify
-    # the positional elements.  As usual, these are in the coordinate frame named in frame_id.  Every time the
-    # marker is displayed in rviz, ROS will use tf to determine where the marker should appear in the scene.
-    # in this case, the position will always be directly above the robot, and will move with it.
-    sphere.pose.position.x = x
-    sphere.pose.position.y = y
-    sphere.pose.position.z = z
-    sphere.pose.orientation.x = 0.0
-    sphere.pose.orientation.y = 0.0
-    sphere.pose.orientation.z = 0.0
-    sphere.pose.orientation.w = 1.0
-
-    self.proxy_markers.markers.append(sphere)
-    # Set up a publisher.  We're going to publish on a topic called balloon.
-    self.markerPublisher.publish(self.proxy_markers)
-
-    # Set a rate.  10 Hz is a good default rate for a marker moving with the Fetch robot.
-    rate = rospy.Rate(10)
+# def place_marker_text(pos=[0, 0, 0], scale=0.01, text='caption', cframe='world'):
+#     """
+#     Places text as marker in RVIZ
+#     @param pos: x,y,z location
+#     @param scale: scale of the text
+#     @param text: text to display
+#     @param cframe: cframe on which the coordinates are given
+#     @return:
+#     """
+#     # Create a marker.  Markers of all shapes share a common type.
+#     caption = Marker()
+#
+#     # Set the frame ID and type.  The frame ID is the frame in which the position of the marker
+#     # is specified.  The type is the shape of the marker, detailed on the wiki page.
+#     caption.header.frame_id = cframe
+#     caption.type = caption.TEXT_VIEW_FACING
+#
+#     # Each marker has a unique ID number.  If you have more than one marker that you want displayed at a
+#     # given time, then each needs to have a unique ID number.  If you publish a new marker with the same
+#     # ID number and an existing marker, it will replace the existing marker with that ID number.
+#     caption.id = 0
+#
+#     # Set the action.  We can add, delete, or modify markers.
+#     caption.action = caption.ADD
+#
+#     # These are the size parameters for the marker.  The effect of these on the marker will vary by shape,
+#     # but, basically, they specify how big the marker along each of the axes of the coordinate frame named
+#     # in frame_id.
+#     caption.scale.x = scale
+#     caption.scale.y = scale
+#     caption.scale.z = scale
+#
+#     # Color, as an RGB triple, from 0 to 1.
+#     caption.color.r = 0
+#     caption.color.g = 0
+#     caption.color.b = 0
+#     caption.color.a = 1
+#
+#     caption.text = text
+#
+#     # Specify the pose of the marker.  Since spheres are rotationally invarient, we're only going to specify
+#     # the positional elements.  As usual, these are in the coordinate frame named in frame_id.  Every time the
+#     # marker is displayed in rviz, ROS will use tf to determine where the marker should appear in the scene.
+#     # in this case, the position will always be directly above the robot, and will move with it.
+#     caption.pose.position.x = pos[0]
+#     caption.pose.position.y = pos[1]
+#     caption.pose.position.z = pos[2]
+#
+#     # Set up a publisher.  We're going to publish on a topic called balloon.
+#     self.marker_text_publisher.publish(caption)
+#
+#     # Set a rate.  10 Hz is a good default rate for a marker moving with the Fetch robot.
+#     rate = rospy.Rate(10)
+#
+#
+# def place_marker(color=[0, 0, 1, 1], pos=[0, 0, 0], scale=0.01, cframe='world'):
+#     """
+#     Creates a Sphere as Marker, and appends it into an array of markers
+#     @param color: [r,g,b,a] where rgb are color i RGB format, and 'a' is visibility
+#     @param pos: [x,y,z] coordinates of the center of the marker
+#     @param scale:
+#     @param cframe: coordinate frame
+#     @return:
+#     """
+#     # Create a marker.  Markers of all shapes share a common type.
+#     sphere = Marker()
+#
+#     # Set the frame ID and type.  The frame ID is the frame in which the position of the marker
+#     # is specified.  The type is the shape of the marker, detailed on the wiki page.
+#     sphere.header.frame_id = cframe
+#     sphere.type = sphere.SPHERE
+#
+#     # Each marker has a unique ID number.  If you have more than one marker that you want displayed at a
+#     # given time, then each needs to have a unique ID number.  If you publish a new marker with the same
+#     # ID number and an existing marker, it will replace the existing marker with that ID number.
+#     sphere.id = self.marker_id + 1
+#     self.marker_id = sphere.id
+#
+#     # Set the action.  We can add, delete, or modify markers.
+#     sphere.action = sphere.ADD
+#
+#     # These are the size parameters for the marker.  The effect of these on the marker will vary by shape,
+#     # but, basically, they specify how big the marker along each of the axes of the coordinate frame named
+#     # in frame_id.
+#     sphere.scale.x = scale
+#     sphere.scale.y = scale
+#     sphere.scale.z = scale
+#
+#     # Color, as an RGB triple, from 0 to 1.
+#     sphere.color.r = color[0]
+#     sphere.color.g = color[1]
+#     sphere.color.b = color[2]
+#     sphere.color.a = color[3]
+#
+#     # Specify the pose of the marker.  Since spheres are rotationally invarient, we're only going to specify
+#     # the positional elements.  As usual, these are in the coordinate frame named in frame_id.  Every time the
+#     # marker is displayed in rviz, ROS will use tf to determine where the marker should appear in the scene.
+#     # in this case, the position will always be directly above the robot, and will move with it.
+#     sphere.pose.position.x = x
+#     sphere.pose.position.y = y
+#     sphere.pose.position.z = z
+#     sphere.pose.orientation.x = 0.0
+#     sphere.pose.orientation.y = 0.0
+#     sphere.pose.orientation.z = 0.0
+#     sphere.pose.orientation.w = 1.0
+#
+#     self.proxy_markers.markers.append(sphere)
+#     # Set up a publisher.  We're going to publish on a topic called balloon.
+#     self.markerPublisher.publish(self.proxy_markers)
+#
+#     # Set a rate.  10 Hz is a good default rate for a marker moving with the Fetch robot.
+#     rate = rospy.Rate(10)
 
 
 # ---------------- ROSBAG FUNCTIONS AND METHODS --------------------- #
@@ -185,7 +189,7 @@ def stop_rosbag(cmd, process):
 
 
 def bag_to_csvs(file):
-    """Open all bagfiles in a folder and saves all topics as csvs"""
+    """Opens bagfile and saves all topics (except image topics) as csvs"""
 
     if file.endswith(".bag"):
         print(file)
@@ -203,6 +207,38 @@ def bag_to_csvs(file):
                 data = bag.message_by_topic(topic)
             else:
                 pass
+
+
+def bag_to_pngs(input_dir, bag_file, cam_topic):
+    """Method to extract images from a bagfile, it saves the images into a PNGS folder"""
+
+    # Create folder to save pngs
+    only_filename = bag_file.split(".bag")[0]
+    output_dir = input_dir + only_filename + "/pngs"
+
+    bag = rosbag.Bag(input_dir + bag_file, "r")
+    bridge = CvBridge()
+
+    count = 0
+    initial_time_stamp = 0.0
+    for topic, msg, t in bag.read_messages(topics=[cam_topic]):
+
+        cv_img = bridge.imgmsg_to_cv2(msg, "bgr8")
+
+        # Add elapsed time as text
+        if count == 0:
+            initial_time_stamp = t
+        elapsed_time = round((t.to_sec() - initial_time_stamp.to_sec()), 3)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(cv_img, 'time [sec]: ' + str(elapsed_time), (int(cv_img.shape[0] * 0.05), int(cv_img.shape[1] * 0.73)), font, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+
+        # Save file
+        cv2.imwrite(os.path.join(output_dir, str(int(elapsed_time*1000)) + ".png"), cv_img)
+        print("Wrote image %i" % count)
+
+        count += 1
+
+    bag.close()
 
 
 # ---------------- FILE NAMING CONVENTION ---------------------------- #
