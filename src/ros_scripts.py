496e9fae94e8c08408a33c3936cb6de7116d6a98
Alejo <velasale@oregonstate.edu>
2023-12-01 18:34:24 -0800

    open a batch of bagfiles

--- src/ros_scripts.py
+++ src/ros_scripts.py
@@ -6,6 +6,7 @@ import subprocess, shlex, psutil
 import os
 from bagpy import bagreader
 import datetime
+from pathlib import Path
 
 # --- 3rd party imports
 import rospy
@@ -307,6 +308,7 @@ def bag_to_video(input_dir, bag_file, cam_topic):
         count += 1
 
     bag.close()
+
     out.release()
 
 
@@ -350,19 +352,50 @@ def main():
     # --- files used for ICRA24 media ---
     file = '20230731_proxy_sample_6_yaw_45_rep_0_stiff_low_force_low.bag'
     file = '20230922_realapple3_attempt_1_orientation_0_yaw_0.bag'
-
     file = '20230922_realapple2_attempt_1_orientation_0_yaw_0.bag'
 
-    # --- Step 2: You may save the png files as well ---
-    topic = "/camera/image_raw"
-    bag_to_pngs(folder + subfolder, file, topic,'/pngs_inhand_cam')
+    # file = '2023111_realapple21_mode_dual_attempt_3_orientation_0_yaw_0.bag'
 
-    topic = "/usb_cam/image_raw"
-    bag_to_pngs(folder + subfolder, file, topic, '/pngs_fixed_cam')
+    # --- Data Location ---
+    folder = '/media/alejo/Elements/Prosser_Data/'
+    subfolder = 'Dataset - apple grasps/'
 
-    # bag_to_csvs(folder + file)
-    bag_to_video(folder + subfolder, file, topic)
-    # -------------------------------------
+    # --- Opening bags in a batch ---
+    for filename in os.listdir(folder + subfolder):
+        if filename.endswith(".bag"):
+            print(filename)
+
+            # --- Skip if there is already a folder
+            just_name = filename.split('.', 1)[0]
+            print(just_name)
+            if Path(folder + subfolder + just_name).is_dir():
+                print('there is already a folder with this name')
+                continue
+
+            # --- Extract topics
+            print('there is not a folder yet')
+            topic = "/camera/image_raw"
+            bag_to_pngs(folder + subfolder, filename, topic,'/pngs_inhand_cam')
+
+            topic = "/usb_cam/image_raw"
+            bag_to_pngs(folder + subfolder, filename, topic, '/pngs_fixed_cam')
+            bag_to_video(folder + subfolder, filename, topic)
+
+            bag_to_csvs(folder + subfolder + filename)
+
+    #
+    # file = '2023111_realapple21_mode_dual_attempt_3_orientation_0_yaw_0.bag'
+    #
+    # # --- Step 2: You may save the png files as well ---
+    # topic = "/camera/image_raw"
+    # bag_to_pngs(folder + subfolder, file, topic,'/pngs_inhand_cam')
+    #
+    # topic = "/usb_cam/image_raw"
+    # bag_to_pngs(folder + subfolder, file, topic, '/pngs_fixed_cam')
+    #
+    # bag_to_csvs(folder + subfolder + file)
+    # bag_to_video(folder + subfolder, file, topic)
+    # # -------------------------------------
 
 
 
