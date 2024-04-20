396a3e7c160d8b3a6176b43d14f36f8bc5626e0a
Alejo <velasale@oregonstate.edu>
2024-04-19 17:30:21 -0700

    tinkering IMU

--- temp/imu_data_reader.py
+++ temp/imu_data_reader.py
@@ -2,6 +2,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
+# Sources
+# https://learn.adafruit.com/adafruit-bno055-absolute-orientation-sensor/overview
+
+
 # todos
 # Hardware: * Make it wireless
 #           * Place battery
@@ -34,18 +38,21 @@ gra_x = df.iloc[:, 17].astype(int)
 plt.plot(gyr_x, label='gyro_X')
 plt.plot(gyr_y, label='gyro_Y')
 plt.plot(gyr_z, label='gyro_Z')
+plt.ylabel('Angular Velocity [rad/s]')
 plt.legend()
 
 fig = plt.figure()
 plt.plot(lin_x, label='lin_X')
 plt.plot(lin_y, label='lin_Y')
 plt.plot(lin_z, label='lin_Z')
+plt.ylabel('Linear Acceleration (minus gravity) [m/s^2]')
 plt.legend()
 
 fig = plt.figure()
 plt.plot(acc_x, label='acc_X')
 plt.plot(acc_y, label='acc_Y')
 plt.plot(acc_z, label='acc_Z')
+plt.ylabel('Acceleration [m/s^2]')
 plt.legend()
 
 
