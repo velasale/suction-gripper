dcb89d37651d629b4a0fe8c6a9d8795957341fdd
Alejo <velasale@oregonstate.edu>
2024-04-21 20:01:41 -0700

    concepts

--- temp/datascience_concepts.py
+++ temp/datascience_concepts.py
@@ -3,17 +3,26 @@ import matplotlib.pyplot as plt
 import random
 
 def kalman_filter_example():
+    # Example of measuring a fixed value variable:
+    # e.g. Voltage sensor of a power supply set at a constant value
+    # e.g. Altitude sensor of a building with the a constant height
 
     # --- Create a noisy signal
     size = 1000
     sigma = 0.2
-    value = 5
-    voltage = 5*np.ones(size)       # Real values
-    noise = np.random.normal(value, sigma, size)    # Noisy values
+    amplitude = 5
+    periods = 1
+    # Constant signal
+    voltage = amplitude * np.ones(size)       # Real values
+    # Sinusoidal signal
+    x = np.linspace(-periods * np.pi, periods * np.pi, size)
+    voltage = np.add(5, np.sin(x))
+    # Measurement Noise
+    noise = np.random.normal(0, sigma, size)    # Noisy values
     plt.hist(noise)
-
+    measurement = np.add(voltage, noise)
     # Turn into list
-    noise_list = noise.tolist()
+    meas_list = measurement.tolist()
 
     # State Estimates
     xE = []
@@ -21,9 +30,12 @@ def kalman_filter_example():
     # Error Covariance
     P = []
     P.append(1)
+    # Store K
+    Ks = []
 
     Q = 1e-5            # Process noise covariance
     R = sigma ** 2      # Measurement noise covariance
+    R = 0.001
 
     for i in range(1, size):
         # Predict
@@ -31,18 +43,20 @@ def kalman_filter_example():
 
         # Correct
         K = error / (error + R)
-        prediction = xE[-1] + K*(noise[i]-xE[-1])
+        Ks.append(K)
+        prediction = xE[-1] + K*(meas_list[i]-xE[-1])
         xE.append(prediction)
 
         new_error = (1-K) * error
         P.append(new_error)
 
     fig = plt.figure()
-    plt.plot(noise, label='Measurement')
+    plt.plot(meas_list, label='Measurement', marker=".", color='r', linestyle='None', markersize = 2.0)
     plt.plot(xE, label='Filtered signal')
     plt.plot(voltage, label='Real value')
+    plt.plot(Ks, label='Kalman Gain')
     plt.legend()
-    plt.ylim([4.5, 5.5])
+    # plt.ylim([3, 6])
     plt.show()
 
 if __name__ == '__main__':
